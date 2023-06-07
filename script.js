90a854c7f63c3972c59b5ca57223e0f7b90b64f6
Maisie O'Brien-Butcher <maisieobb@googlemail.com>
2023-06-07 16:46:38 +0100

    commit changes

---
+++ script.js
@@ -0,0 +1,60 @@
+window.onload = function () {
+  let display = document.querySelector(".output-number");
+  let operators = ["*", "/", "x", "-", "+", "="];
+  let output = " ";
+  let buttons = document.querySelectorAll(".calculator-btn");
+  let toggle1 = document.querySelector(".toggle-1");
+  let toggle2 = document.querySelector(".toggle-2");
+  let toggle3 = document.querySelector(".toggle-3");
+  let body = document.querySelector("body");
+
+  toggle1.addEventListener("click", () => {
+    body.classList.remove("theme-2", "theme-3");
+      body.classList.add("theme-1");
+      console.log(body.classList)
+  });
+
+  toggle2.addEventListener("click", () => {
+    body.classList.remove("theme-1", "theme-3");
+    body.classList.add("theme-2");
+  });
+
+  toggle3.addEventListener("click", () => {
+    body.classList.remove("theme-1", "theme-2");
+    body.classList.add("theme-3");
+  });
+
+  /* Function to perform calculations with various button clicks */
+  let calculate = (btnValue) => {
+    switch (btnValue) {
+      case "=":
+        if (output !== "") {
+          output = eval(output);
+        }
+        break;
+      case "RESET":
+        output = "0";
+        break;
+      case "DEL":
+        output = output.toString().slice(0, -1);
+        break;
+
+      default:
+        if (operators.includes(btnValue)) {
+          if (output === "" || operators.includes(output.slice(-1))) {
+            return;
+          }
+        }
+        output += btnValue;
+        break;
+    }
+    display.textContent = output || 0;
+  };
+
+  /*Add a click event listener to each button that calls the calculate function */
+  buttons.forEach((button) => {
+    button.addEventListener("click", (e) => {
+      calculate(e.target.textContent);
+    });
+  });
+};
